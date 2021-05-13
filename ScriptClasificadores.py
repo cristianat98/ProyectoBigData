a52675718283e56e42de7593da6d7cbe5a5286c1
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2021-05-13 11:14:27 +0200

    'ScriptClasificadoresNoAcabado'

--- ScriptClasificadores.py
+++ ScriptClasificadores.py
@@ -21,3 +21,32 @@ teams = pd.read_csv("teams_csv.csv")
 matches = pd.read_csv("matches_csv.csv")
 
 #=========================================================================
+
+#Separación de los datos para el entrenamiento
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+
+#Inicialización de clasificadores para comparar
+names = ["KNN5", "KNN10", "Decision Tree 8", "Decision Tree 16", "GNB", "SVC Linear", "SVC Polynomial", "Neural Net"]
+classifiers = [
+    KNeighborsClassifier(metric='minkowski', n_neighbors=5),
+    KNeighborsClassifier(metric='minkowski', n_neighbors=10),
+    DecisionTreeClassifier(criterion='entropy', max_depth=8, random_state=1),
+    DecisionTreeClassifier(criterion='entropy', max_depth=16, random_state=1),
+    GaussianNB(),
+    SVC(kernel="linear"),
+    SVC(kernel="poly", gamma=0.20, C=1.0),
+    MLPClassifier(alpha=1, max_iter=1000)]
+
+#==========================================================
+
+#Cross-validation y muestra de resultados 
+
+#cv_accuracy = dict()
+#for name, clf in zip(names, classifiers):
+    # One metric: score (accuracy)
+    # Perform 10-fold cross-validation
+#    cv_scores = cross_val_score(clf, X, y, cv=10)
+#    cv_accuracy[name] = cv_scores.mean()
+#    print(name)
+#    print("Cross-Validation Accuracy: %0.4f (+/- %0.2f)" % (cv_scores.mean(), cv_scores.std() * 2))
