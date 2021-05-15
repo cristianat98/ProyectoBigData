913ccf1dbb06f8f55dbe184e158a12f2b9eea532
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2021-05-15 13:20:13 +0200

    'DiferentesClasificadores'

--- ScriptClasificadores.py
+++ ScriptClasificadores.py
@@ -1,6 +1,6 @@
 #Script de clasificadores
 
-#======================================================================
+#===============================================================================================================================
 #Importación de librerias
 
 import pandas as pd
@@ -30,14 +30,15 @@ y = matches['Resultado']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
-knn_model = KNeighborsClassifier(metric='minkowski', n_neighbors=5)
-knn_model.fit(X_train, y_train)
+#knn_model = KNeighborsClassifier(metric='minkowski', n_neighbors=5)
+#knn_model.fit(X_train, y_train)
 
 # test prediction
-y_pred = knn_model.predict(X_test)
-print('Misclassified samples: %d' % (y_test != y_pred).sum())
-print('Accuracy: %.2f%%' % (100.0 * knn_model.score(X_test, y_test)))
+#y_pred = knn_model.predict(X_test)
+#print('Misclassified samples: %d' % (y_test != y_pred).sum())
+#print('Accuracy: %.2f%%' % (100.0 * knn_model.score(X_test, y_test)))
 
+#===============================================================================================================================
 
 #Inicialización de clasificadores para comparar
 names = ["KNN5", "KNN10", "Decision Tree 8", "Decision Tree 16", "GNB", "SVC Linear", "SVC Polynomial", "Neural Net"]
@@ -51,15 +52,15 @@ classifiers = [
     SVC(kernel="poly", gamma=0.20, C=1.0),
     MLPClassifier(alpha=1, max_iter=1000)]
 
-#==========================================================
+#==============================================================================================================================
 
 #Cross-validation y muestra de resultados 
 
-#cv_accuracy = dict()
-#for name, clf in zip(names, classifiers):
-    # One metric: score (accuracy)
-    # Perform 10-fold cross-validation
-#    cv_scores = cross_val_score(clf, X, y, cv=10)
-#    cv_accuracy[name] = cv_scores.mean()
-#    print(name)
-#    print("Cross-Validation Accuracy: %0.4f (+/- %0.2f)" % (cv_scores.mean(), cv_scores.std() * 2))
+cv_accuracy = dict()
+for name, clf in zip(names, classifiers):
+    #   One metric: score (accuracy)
+    #   Perform 10-fold cross-validation
+    cv_scores = cross_val_score(clf, X, y, cv=10)
+    cv_accuracy[name] = cv_scores.mean()
+    print(name)
+    print("Cross-Validation Accuracy: %0.4f (+/- %0.2f)" % (cv_scores.mean(), cv_scores.std() * 2))
