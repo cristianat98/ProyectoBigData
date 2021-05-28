6680833a129ef6858600b0fbc67ab31047ab8866
cristianat98 <cristianarmestotejedo@gmail.com>
2021-05-28 09:43:29 +0200

    ---

--- ScriptClasificadores.py
+++ ScriptClasificadores.py
@@ -29,6 +29,10 @@ X = matches.drop(['HomeTeam','AwayTeam','Resultado'],axis=1)
 y = matches['Resultado']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+X_train = X.iloc[0:1139]
+y_train = y.iloc[0:1139]
+X_test = X.iloc[1140:1519]
+y_test = y.iloc[1140:1519]
 
 #knn_model = KNeighborsClassifier(metric='minkowski', n_neighbors=5)
 #knn_model.fit(X_train, y_train)
@@ -49,9 +53,8 @@ classifiers = [
     DecisionTreeClassifier(criterion='entropy', max_depth=8, random_state=1),
     DecisionTreeClassifier(criterion='entropy', max_depth=16, random_state=1),
     GaussianNB(),
-    SVC(kernel="linear"),
-    SVC(kernel="poly", gamma=0.20, C=1.0),
-    MLPClassifier(alpha=1, max_iter=1000)]
+    #SVC(kernel="linear"),
+    #SVC(kernel="poly", gamma=0.20, C=1.0),
 
 #==============================================================================================================================
 
